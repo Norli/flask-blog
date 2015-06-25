28d490a70232ba67b70aad3e530cddd5b3675be4
Norli <vilblirik@yahoo.no>
2015-06-25 16:30:40 +0000

    Lagt til login

--- blog.py
+++ blog.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 import sqlite3
 
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'fuckme'
+
 app = Flask(__name__)
 
 app.config.from_object(__name__)
@@ -11,7 +15,21 @@ def connect_db():
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['PASSWORD'] or \
+            request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 @app.route('/main')
 def main():
